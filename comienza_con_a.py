749d2e7d8032946ee19a3d493a9c7da2d526f3df
jzmx <jorge@zaldivar.mx>
2023-10-10 01:50:39 +0000

    First try at solution (no lower case, no dyacritics)

--- comienza_con_a.py
+++ comienza_con_a.py
@@ -1,15 +1,15 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Comienza con A
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+palabra = input("Escribe una palabra: ")
 
 # Proceso
-
+if palabra[0] == "A":
+    comienza = "comienza"
+else:
+    comienza = "no comienza"
 
 # Salidas
-print(salida)
+print(f"'{palabra}' {comienza} con 'A'")
